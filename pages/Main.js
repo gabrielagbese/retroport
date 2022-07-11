8195f149ee31fc816482dcbab0a6b7a1e60c0e14
therealbeez <gabriel.agbese2@gmail.com>
2022-07-11 21:00:39 +0200

    created basic structual layout

--- pages/Main.js
+++ pages/Main.js
@@ -1,7 +1,14 @@
-export default function Hello() {
+export default function Main() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="container">
+      <div className="hero">a</div>
+      <div className="cards">
+        <div className="card about">about</div>
+        <div className="card projects">projects</div>
+        <div className="card contact">contact</div>
+      </div>
+      <div className="content">content</div>
+      
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Hello from './Main.js'
+import Main from './Main.js'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <Hello />
+    <Main />
   )
 }
